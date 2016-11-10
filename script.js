c9597be43229d7d6c684cad47fcdf9c41a2794ac
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-09 17:07:10 -0700

    beginning of logic

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ var test = function() {
 console.log("JS Test");
 };
 console.log(test);
+
+document.getElementbyId('#box1').addEventListener("click",)
+
+var clickCount = function() {
+	var clicks = 0; 
+return function(){
+	clicks++;
+};
+};
+document.addEventListener("click",onClick);
