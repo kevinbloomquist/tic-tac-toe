ba7ccc0a9c9c0d45d9b8674aa5981959023e5e84
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-09 18:58:25 -0700

    end of evening

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ console.log("JS Test");
 };
 console.log(test);
 
-document.getElementbyId('#box1').addEventListener("click",)
+// hold this for after resolution of click count: (will most likely have to do for each box)
+// css x state = even; css o state = odd? ie if onClick or clickCount = odd add attribute oState
+// document.getElementbyId('#box1').addEventListener("click",onClick())
 
-var clickCount = function() {
+var onClick = function() {
 	var clicks = 0; 
 return function(){
 	clicks++;
