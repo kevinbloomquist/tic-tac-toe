26b12423809c3a2988cc5abcbab5316b59783372
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-11 14:42:52 -0700

    linked and converted file to JQ to get around function problem

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 var test = function() {
-console.log("JS Test");
+console.log("new refresh");
 };
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
 console.log(test);
 // listen for click and on click run the counter function advancing the counter
 var boardClick = document.getElementsByClassName("grid");
-
 console.log(boardClick);
-boardClick.addEventListener("click",counter);
+
 var currentCount = 0;
 var counter  = function(){
 	currentCount++;
 };
+counter();
+console.log(currentCount);
+// ??????????????????????????????????????????????????????????????????????????????????????????
+var boardClick = document.getElementById("grid");
+console.log(boardClick);
+$(boardClick).on("click",counter);
+// boardClick.addEventListener("click",counter);
+// var currentCount = 0;
+// var counter  = function(){
+	// currentCount++;
+// };
 
 
 console.log(currentCount);
 // ChangeState defines how to change the css of a specific box(div) once clicked.
-// Once tested and fnctional figure out how to use .this to dry out the methodology.
+// Once tested and functional maybe figure out how to use .this to dry out the methodology.
 var changeState = function(){
 	if(counter%2===0) {
 		document.getElementById("#box1").setAttribute("class","clicked").innerHTML("<p>X</p>");
@@ -23,17 +38,17 @@ var changeState = function(){
 		document.getElementById("#box1").setAttribute("class","clicked").innerHTML("<p>O</p>");
 	}
 };
-console.log(changeState());
+console.log(changeState);
 //Adds event listener to each div and runs the changeState function to create x or o
-document.getElementById("box1").addEventListener("click",changeState);
-document.getElementById("box2").addEventListener("click",changeState);
-document.getElementById("box3").addEventListener("click",changeState);
-document.getElementById("box4").addEventListener("click",changeState);
-document.getElementById("box5").addEventListener("click",changeState);
-document.getElementById("box6").addEventListener("click",changeState);
-document.getElementById("box7").addEventListener("click",changeState);
-document.getElementById("box8").addEventListener("click",changeState);
-document.getElementById("box9").addEventListener("click",changeState);
+$("#box1").on("click",changeState);
+$("#box2").on("click",changeState);
+$("#box3").on("click",changeState);
+$("#box4").on("click",changeState);
+$("#box5").on("click",changeState);
+$("#box6").on("click",changeState);
+$("#box7").on("click",changeState);
+$("#box8").on("click",changeState);
+$("#box9").on("click",changeState);
 
 // Add winState.
 // var winState = function(){
