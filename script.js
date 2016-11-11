c0d0add6277e03dc54cb6450007bee5aa7e3949e
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-10 20:23:20 -0700

    adds comments and looks ahead to winState

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ var test = function() {
 console.log("JS Test");
 };
 console.log(test);
+// listen for click and on click run the counter function advancing the counter
+var boardClick = document.getElementByClass("grid").AddEventListener("click",counter);
+var counter = function(){
 
-var counter = 
-// hold this for after resolution of click count: (will most likely have to do for each box)
-// css x state = even; css o state = odd? ie if onClick or clickCount = odd add attribute oState
-// document.getElementbyId('#box1').addEventListener("click",onClick())
+};
+console.log(counter);
+// ChangeState defines how to change the css of a specific box(div) once clicked.
+// Once tested and fnctional figure out how to use .this to dry out the methodology
+var changeState = function(){
+	if(counter%2===0) {
+		document.getElementById("#box1").setAttribute("class","clicked").innerHTML("<p>X</p>");
+	} else if (counter%2===1) {
+		document.getElementById("#box1").setAttribute("class","clicked").innerHTML("<p>O</p>");
+	}
+};
+console.log(changeState());
+//Adds event listener to each div and runs the changeState function to create x or o
 document.getElementById("box1").addEventListener("click",changeState);
 document.getElementById("box2").addEventListener("click",changeState);
 document.getElementById("box3").addEventListener("click",changeState);
@@ -16,10 +28,7 @@ document.getElementById("box6").addEventListener("click",changeState);
 document.getElementById("box7").addEventListener("click",changeState);
 document.getElementById("box8").addEventListener("click",changeState);
 document.getElementById("box9").addEventListener("click",changeState);
-var changeState = function(){
-	if(counter%2===0) {
-		document.getElementById("#box1").setAttribute("class","clicked").innerHTML("<p>X</p>");
-	} else if (counter%2===1) {
-		document.getElementById("#box1").setAttribute("class","clicked").innerHTML("<p>O</p>");
-	}
-};
+
+// Add Win state.
+// if(box 123||or box 456||or159...)
+
