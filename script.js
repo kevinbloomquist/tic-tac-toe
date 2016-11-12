2c37e4e758777006069511e378ce26a808a1ff42
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-12 15:29:28 -0700

    ripped code apart. changeState CSS works in box1 test case. Cannot register click in any way nor can I advance counter or get div to respond

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ $( document ).ready(function() {
 
 console.log(test);
 // listen for click and on click run the counter function advancing the counter
-var boardClick = document.getElementsByClassName("grid");
-console.log(boardClick);
+var clickField = document.getElementsByClassName("grid");
+console.log(clickField);
 
-var currentCount = 0;
-var counter  = function(){
+var currentCount = 1;
+var counter  = function(currentCount){
 	currentCount++;
 };
 counter();
 console.log(currentCount);
 // ??????????????????????????????????????????????????????????????????????????????????????????
-var boardClick = document.getElementById("grid");
-console.log(boardClick);
-$(boardClick).on("click",counter);
-// boardClick.addEventListener("click",counter);
+var clickField = document.getElementById("grid");
+console.log(clickField);
+$(clickField).on("click",counter(currentCount));
+// clickField.addEventListener("click",counter);
 // var currentCount = 0;
 // var counter  = function(){
 	// currentCount++;
@@ -29,27 +29,30 @@ $(boardClick).on("click",counter);
 
 
 console.log(currentCount);
+var divCheck =document.getElementById("box1");
+console.log(divCheck);
 // ChangeState defines how to change the css of a specific box(div) once clicked.
 // Once tested and functional maybe figure out how to use .this to dry out the methodology.
-var changeState = function(){
+var changeState = function(currentCount){
 	if(currentCount%2===0) {
-		$("#box1").removeClass("box");
-		this.addClass("clicked");
-		this.html("<p>X</p>");
+		$("#box1").removeClass("box").addClass("clicked").html("<p>X</p>");
 	} else if (currentCount%2===1) {
-		$("#box1").removeClass("box");
-		this.addClass("clicked");
-		this.html("<p>O</p>");
+		$("#box1").removeClass("box").addClass("clicked").html("<p>O</p>");
 	}
-	console.log(currentCount);
 	// checkForWin();
 };
-console.log(changeState);
+console.log(changeState(currentCount));
 //Adds event listener to each div and runs the changeState function to create x or o
-$("#box1").on("click", function(){
-    alert("The paragraph was clicked.");
+$("#box1").on("Click",function(currentCount){
+	if(currentCount%2===0) {
+		$("#box1").removeClass("box").addClass("clicked").html("<p>X</p>");
+	} else if (currentCount%2===1) {
+		$("#box1").removeClass("box").addClass("clicked").html("<p>O</p>");
+	}
+	// checkForWin();
 });
-$("#box2").on("click",changeState);
+
+$("#box2").on("Click",changeState('#box2'));
 $("#box3").on("click",changeState);
 $("#box4").on("click",changeState);
 $("#box5").on("click",changeState);
