21bc9df3ca844f5547a26c287fc325f48a9d29ab
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-14 16:02:17 -0700

    fixes box7 css issues

--- clean.js
+++ clean.js
@@ -55,8 +55,66 @@ var changeBox4 = function(counter){
 	console.log(counter);
 };
 
+var changeBox5 = function(counter){
+	  if(counter!==0 && counter%2===0) {
+		$("#box5").removeClass("box").addClass("clicked").html("<p>X</p>");
+	} else if (counter!==0 && counter%2===1) {
+		$("#box5").removeClass("box").addClass("clicked").html("<p>O</p>");
+	}
+	advanceCounter();
+	console.log(counter);
+};
+
+var changeBox6 = function(counter){
+	  if(counter!==0 && counter%2===0) {
+		$("#box6").removeClass("box").addClass("clicked").html("<p>X</p>");
+	} else if (counter!==0 && counter%2===1) {
+		$("#box6").removeClass("box").addClass("clicked").html("<p>O</p>");
+	}
+	advanceCounter();
+	console.log(counter);
+};
+
+var changeBox7 = function(counter){
+	  if(counter!==0 && counter%2===0) {
+		$("#box7").removeClass("box").addClass("clicked").html("<p>X</p>");
+	} else if (counter!==0 && counter%2===1) {
+		$("#box7").removeClass("box").addClass("clicked").html("<p>O</p>");
+	}
+	advanceCounter();
+	console.log(counter);
+};
+
+var changeBox8 = function(counter){
+	  if(counter!==0 && counter%2===0) {
+		$("#box8").removeClass("box").addClass("clicked").html("<p>X</p>");
+	} else if (counter!==0 && counter%2===1) {
+		$("#box8").removeClass("box").addClass("clicked").html("<p>O</p>");
+	}
+	advanceCounter();
+	console.log(counter);
+};
+
+var changeBox9 = function(counter){
+	  if(counter!==0 && counter%2===0) {
+		$("#box9").removeClass("box").addClass("clicked").html("<p>X</p>");
+	} else if (counter!==0 && counter%2===1) {
+		$("#box9").removeClass("box").addClass("clicked").html("<p>O</p>");
+	}
+	advanceCounter();
+	console.log(counter);
+};
+
 document.getElementById('box1').addEventListener('click',function (){changeBox1(counter);});
 document.getElementById('box2').addEventListener('click',function (){changeBox2(counter);});
+document.getElementById('box3').addEventListener('click',function (){changeBox3(counter);});
+document.getElementById('box4').addEventListener('click',function (){changeBox4(counter);});
+document.getElementById('box5').addEventListener('click',function (){changeBox5(counter);});
+document.getElementById('box6').addEventListener('click',function (){changeBox6(counter);});
+document.getElementById('box7').addEventListener('click',function (){changeBox7(counter);});
+document.getElementById('box8').addEventListener('click',function (){changeBox8(counter);});
+document.getElementById('box9').addEventListener('click',function (){changeBox9(counter);});
+
 
 	
 
