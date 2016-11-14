e769b3395e7503ce793ab87d7916e91939340b60
kevinbloomquist <rkevinbloom@gmail.com>
2016-11-14 11:37:58 -0700

    changeState makes change but need to register click event and account for beginning state

--- clean.js
+++ clean.js
@@ -6,23 +6,23 @@ $(document).ready(function() {
     console.log( "ready!" );
 });
 
-var element = document.getElementsByClassName("grid");
+var element = document.getElementById("box1");
 console.log(element);
 
-var counter = 1;
+var counter = 0;
 var advanceCounter = function(){
   counter += 1;
   console.log("clicked " + counter + " times.");
 };
 
-var changeBox1 = function(a){
-	if(a%2===0) {
+var changeBox1 = function(counter){
+	  if(counter%2===0) {
 		$("#box1").removeClass("box").addClass("clicked").html("<p>X</p>");
-	} else if (a%2===1) {
+	} else if (counter%2===1) {
 		$("#box1").removeClass("box").addClass("clicked").html("<p>O</p>");
 	}
 };
 
-// $("#box1").on("Click",changeBox1());
+document.getElementById('box1').addEventListener('Click',changeBox1(counter));
 	
 
